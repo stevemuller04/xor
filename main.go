1803b5cc47326250b9ca14c31cbf85a5208e76ad
Steve Muller <steve.muller@outlook.com>
2026-03-05 15:00:46 +0100

    Support for cyclic XOR

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -19,30 +20,82 @@ func main() {
 	}
 
 	// Open files for reading
-	readers := make([]*bufio.Reader, n)
-	for i := 0; i < n; i++ {
-		if f, err := os.Open(os.Args[i + 1]); err != nil {
+	readers := make([]CyclicInput, n)
+	for i := range n {
+		if f, err := os.Open(os.Args[i+1]); err != nil {
 			log.Fatal(err)
 			return
 		} else {
 			defer f.Close()
-			readers[i] = bufio.NewReader(f)
+			readers[i] = NewInput(f)
 		}
 	}
 
 	// XOR all the bytes
 	for {
 		var result byte = 0
-		for i := 0; i < n; i++ {
-			if b, err := readers[i].ReadByte(); err == io.EOF {
-				return
-			} else if err != nil {
+		var allEOF bool = true
+
+		for i := range n {
+			if err := readers[i].ReadNextByte(); err != nil {
 				log.Fatal(err)
-				return
-			} else {
+			} else if b, ok := readers[i].GetByte(); ok {
 				result ^= b
 			}
+			allEOF = allEOF && readers[i].EOF
 		}
-		f.WriteByte(result)
+
+		if allEOF {
+			break
+		} else {
+			f.WriteByte(result)
+		}
+	}
+}
+
+type CyclicInput struct {
+	reader io.ReadSeeker
+	buffer bytes.Buffer
+	EOF    bool
+}
+
+func NewInput(f io.ReadSeeker) CyclicInput {
+	return CyclicInput{f, bytes.Buffer{}, false}
+}
+
+func (i *CyclicInput) cyclicRead(data []byte) (int, error) {
+	if n, err := i.reader.Read(data); n > 0 {
+		return n, err
+	} else if err != nil && err != io.EOF {
+		return 0, err
+	} else {
+		i.EOF = true
+		if _, err := i.reader.Seek(0, io.SeekStart); err != nil {
+			return 0, err
+		} else {
+			return i.reader.Read(data)
+		}
+	}
+}
+
+func (i *CyclicInput) ReadNextByte() error {
+	if i.buffer.Len() != 0 {
+		return nil // we still have data
+	}
+
+	var data [4096]byte
+	if n, err := i.cyclicRead(data[:]); err != nil && err != io.EOF {
+		return err
+	} else {
+		_, err := i.buffer.Write(data[:n])
+		return err
+	}
+}
+
+func (i *CyclicInput) GetByte() (b byte, ok bool) {
+	if b, err := i.buffer.ReadByte(); err != nil {
+		return 0, false
+	} else {
+		return b, true
 	}
 }
